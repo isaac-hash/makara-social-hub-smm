e43e2f189119b776a76b3e8fee9a5d550f60fcbe
isaac-hash <zikkychukwudulue@gmail.com>
2025-12-11 09:07:30 +0100

    modified:   app/modules/add_funds/views/index.php

--- app/modules/add_funds/views/index.php
+++ app/modules/add_funds/views/index.php
@@ -333,7 +333,7 @@
         src="https://youtube.com/embed/0_T5SuGKpic?si=bGDeQ_gEQBSA4yz8" 
         style="position:absolute; top:0; left:0; width:100%; height:100%;" 
         frameborder="0" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;" 
         allowfullscreen>
     </iframe>
 </div>

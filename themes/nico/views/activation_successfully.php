2b004074b577a591ce88662385edda5318ea0f8f
isaac-hash <zikkychukwudulue@gmail.com>
2025-12-31 18:21:43 +0100

    modified:   themes/nico/views/activation_successfully.php 	modified:   themes/nico/views/change_password.php

--- themes/nico/views/activation_successfully.php
+++ themes/nico/views/activation_successfully.php
@@ -3,23 +3,16 @@
   <?php include_once 'blocks/head.blade.php'; ?>
   <body>
     <main class="d-flex align-items-center min-vh-100 py-3 py-md-0" style="background-color: #0D0BD1">
-      <div class="container">
+      <div class="container col-md-5 mx-auto">
         <div class="card login-card">
-          <div class="row no-gutters">
-            <div class="col-md-6 left-image mx-auto">
-              <img src="<?php echo BASE; ?>themes/nico/assets/images/login.png" alt="login" class="login-card-img">
+          <div class="card-body">
+            <div class="brand-wrapper">
+              <a href="#"><img src="<?=get_option('website_logo', BASE."assets/images/logo.png")?>" alt="logo" class="logo"></a>
             </div>
-            <div class="col-md-6">
-              <div class="card-body">
-                <div class="brand-wrapper">
-                  <a href="#"><img src="<?=get_option('website_logo', BASE."assets/images/logo.png")?>" alt="logo" class="logo"></a>
-                </div>
-                <p class="login-card-description"><?=lang("congratulations_your_registration_is_now_complete")?></p>
-                <p><?=lang('congratulations_desc')?></p>
-                <div class="form-group">
-                  <a class="btn btn-block login-btn mb-4" href="<?=cn('auth/login')?>" type="submit"><?=lang("get_start_now")?></a>
-                </div>
-              </div>
+            <p class="login-card-description" style="color: #fff"><?=lang("congratulations_your_registration_is_now_complete")?></p>
+            <p><?=lang('congratulations_desc')?></p>
+            <div class="form-group">
+              <a class="btn btn-block login-btn mb-4" href="<?=cn('auth/login')?>" type="submit" style="background-color: #0D0BD1; color: #fff"><?=lang("get_start_now")?></a>
             </div>
           </div>
         </div>
--- themes/nico/views/change_password.php
+++ themes/nico/views/change_password.php
@@ -13,18 +13,13 @@
   ?>
   <body>
     <main class="d-flex align-items-center min-vh-100 py-3 py-md-0" style="background-color: #0D0BD1">
-      <div class="container">
+      <div class="container col-md-5 mx-auto">
         <div class="card login-card">
-          <div class="row no-gutters">
-            <div class="col-md-6 left-image mx-auto">
-              <img src="<?php echo BASE; ?>themes/nico/assets/images/login.png" alt="login" class="login-card-img">
+          <div class="card-body">
+            <div class="brand-wrapper">
+              <a href="#"><img src="<?=get_option('website_logo', BASE."assets/images/logo.png")?>" alt="logo" class="logo"></a>
             </div>
-            <div class="col-md-6">
-              <div class="card-body">
-                <div class="brand-wrapper">
-                  <a href="#"><img src="<?=get_option('website_logo', BASE."assets/images/logo.png")?>" alt="logo" class="logo"></a>
-                </div>
-                <p class="login-card-description"><?php echo lang("reset_your_password"); ?></p>
+            <p class="login-card-description"><?php echo lang("reset_your_password"); ?></p>
                 <?php echo form_open($form_url, $form_attributes); ?>
                   <div class="form-group">
                     <input type="password" name="password" id="password" class="form-control" placeholder="<?php echo lang("new_password"); ?>">
@@ -35,11 +30,10 @@
                   <div class="form-group mt-20">
                     <div id="alert-message" class="alert-message-reponse"></div>
                   </div>
-                  <button class="btn btn-block login-btn btn-submit mb-4" type="submit"><?=lang("Submit")?></button>
+                  <button class="btn btn-block login-btn btn-submit mb-4" type="submit" style="background-color: #0D0BD1; color: #fff"><?=lang("Submit")?></button>
                 <?php echo form_close(); ?>
               </div>
             </div>
-          </div>
         </div>
       </div>
     </main>

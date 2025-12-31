d263e7c11d9d7b46cc4257d8a1ea068355e8072a
isaac-hash <zikkychukwudulue@gmail.com>
2025-12-31 18:16:32 +0100

    modified:   themes/nico/views/forgot_password.php

--- themes/nico/views/forgot_password.php
+++ themes/nico/views/forgot_password.php
@@ -15,7 +15,7 @@
     <main class="d-flex align-items-center min-vh-100 py-3 py-md-0" style="background-color: #0D0BD1">
       <div class="container col-md-6 mx-auto">
         <div class="card login-card" style="">
-          <div class="card-body">
+          <div class="card-body mx-auto">
             <div class="brand-wrapper">
               <a href="<?=cn();?>"><img src="<?=get_option('website_logo', BASE."assets/images/logo.png")?>" alt="logo" class="logo"></a>
             </div>
